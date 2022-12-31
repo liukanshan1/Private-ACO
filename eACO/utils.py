326ad3a168199e2a4a5dec1f67599617f61b4023
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-12-31 21:08:36 +0800

    加密numpy数组

--- eACO/utils.py
+++ eACO/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from ss.secret import Secret
+
 # Constants
 fixed_point = 8
 
@@ -9,4 +11,8 @@ def encrypt_nparray(nparray: np.array):
     加密numpy数组
     """
     nparray = (nparray * (10 ** fixed_point)).astype(np.int64)
-    print(nparray)
+    e_nparray = np.empty(nparray.shape, dtype=Secret, order='C')
+    for i in range(nparray.shape[0]):
+        for j in range(nparray.shape[1]):
+            e_nparray[i, j] = Secret(nparray[i, j])
+    return e_nparray
